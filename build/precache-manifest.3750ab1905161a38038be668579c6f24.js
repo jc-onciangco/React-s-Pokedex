af8983c8eef46b009f3fe2b155d3423998be6972
Uwang20 <ohjuan205@gmail.com>
2020-09-09 23:10:30 +0800

    build

---
+++ build/precache-manifest.3750ab1905161a38038be668579c6f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fa31d8ab8eca1fb7981e8e5ef65a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b86f35ff4a438abdc3fd",
+    "url": "/static/css/main.22e03353.chunk.css"
+  },
+  {
+    "revision": "2d8114dc8d875ac6d4f3",
+    "url": "/static/js/2.41d2e1a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41d2e1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86f35ff4a438abdc3fd",
+    "url": "/static/js/main.132201d4.chunk.js"
+  },
+  {
+    "revision": "965aca300182a94c14f3",
+    "url": "/static/js/runtime-main.4126d0af.js"
+  },
+  {
+    "revision": "fd721f81cf12ebf7b3234c2c665b584c",
+    "url": "/static/media/pokeball.fd721f81.png"
+  }
+]);
